334627025f69d95df6a3563dfbbcc68ba499ac41
Yarashev Jonibek <yarashevjonibek@gmail.com>
2026-08-01 22:51:43 +0500

    done

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ set1 = set(input().split())
 set2 = set(input().split())
 
 interection = set1 & set2
-interection.sort(reverse=True)
+sorted_list = sorted(interection)
 display = " ".join(sorted)
 print(display)
