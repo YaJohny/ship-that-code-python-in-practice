992c03816ff7ad4a81e7d228e2520d6a40e89205
Yarashev Jonibek <yarashevjonibek@gmail.com>
2026-08-02 00:05:00 +0500

    done

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def avarage(*args):
-    sum = sum(args)
-    return sum / len(args)
+    args_sum = sum(args)
+    return args_sum / len(args)
 
 n = int(input())
 nums = [float(input()) for _ in range(n)]
