8de9b05cfe0fb9f13e9c653c96a6deb2ac71991c
Yarashev Jonibek <yarashevjonibek@gmail.com>
2026-08-01 23:32:23 +0500

    done

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 def is_palindrome(text):
-    return text == text[::-1]
+    text_list = list(text)
+    reversed_text = text_list[::-1]
+    return text_list == reversed_text
+
 
 text = input()
 if is_palindrome(text):
