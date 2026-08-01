3f4217b8871bf488172dead6e4fc57d065015e0b
Yarashev Jonibek <yarashevjonibek@gmail.com>
2026-08-01 22:37:13 +0500

    done

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-from collections import defaultdict
+set1 = input().split()
+set2 = input().split()
 
-n = int(input())
-students = {name: grade for _ in range(n) for name, grade in [input().split()]}
-grades = defaultdict(list)
-
-for name, grade in students.items():
-    grades[grade].append(name)
-
-for grade, name in grades.items():
-    print(f'{grade}: {", ".join(name)}')
+interection = set1 and set2
+sorted = list(interection).sort(reverse=True)
+display = " ".join(sorted)
