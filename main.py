241656aa16d7202dc567159deed78c48d99d7664
Yarashev Jonibek <yarashevjonibek@gmail.com>
2026-08-02 14:00:34 +0500

    done

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ data_list = [input() for _ in range(n)]
 data = '\n'.join(data_list)
 data_with_header = header + '\n' + data
 
-reader = csv.DictReader(io.input(data_with_header))
+reader = csv.DictReader(io.StringIO(data_with_header))
 total = 0
 for row in reader:
     total += int(row["age"])
