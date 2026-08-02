3469e8d7cf27bced19b1d0614dcb34b1dd415846
Yarashev Jonibek <yarashevjonibek@gmail.com>
2026-08-02 12:30:19 +0500

    done

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 n = int(input())
 nums = {name: int(grade) for _ in range(n) for name, grade in [input().split()]}
-sorted_nums = sorted(nums, key=lambda x: x[1], reverse=True)
+sorted_nums = sorted(nums, key=lambda x: x[name], reverse=True)
 
 for name in sorted_nums:
     print(name)
